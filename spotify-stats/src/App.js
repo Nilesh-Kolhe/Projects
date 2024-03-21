212a42fbc2588a532c28529dfa1d59d68d2818f2
Nilesh Kolhe <nkolhe@vexcelco.com>
2024-03-21 23:25:11 +0530

    Implemented Routing

--- spotify-stats/src/App.js
+++ spotify-stats/src/App.js
@@ -2,22 +2,24 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import NavBar from './components/navbar/NavBar';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import Dashboard from './components/dashboard/Dashboard';
 import Home from './components/home/Home';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <NavBar/>
+      <BrowserRouter>
+        <NavBar />
         <Routes>
-          <Route path="/" element={<Navigate to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="home" element={<Home />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
-      </Router >
+      </BrowserRouter>
     </div >
   );
 }
