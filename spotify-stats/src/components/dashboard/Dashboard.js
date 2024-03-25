271e300a193bf648ba9f103746df36c5fcc834c7
Nilesh Kolhe <nkolhe@vexcelco.com>
2024-03-25 23:04:26 +0530

    Mapped Songs

--- spotify-stats/src/components/dashboard/Dashboard.js
+++ spotify-stats/src/components/dashboard/Dashboard.js
@@ -1,57 +1,69 @@
 import { useSelector } from 'react-redux';
 import './Dashboard.css';
-import { useEffect } from 'react';
-import Card from '../Card/ProfileCard';
+import { useEffect, useState } from 'react';
 
 const Dashboard = () => {
 
-    const { profile } = useSelector(state => state);
+    // const { profile } = useSelector(state => state);
     const { token } = useSelector(state => state);
-    console.log('Dashboard State Token: ', token);
+    const [mostPlayed, setMostPlayed] = useState([]);
 
     const load = async () => {
-        const params = new URLSearchParams();
-        params.append("scope", "user-top-read");
-
         const result = await fetch("https://api.spotify.com/v1/me/top/tracks", {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` }
         });
-        return await result.json();
+        const response = await result.json();
+        console.log('Most Played: ', response);
+        mapToSongState(response);;
+    }
+
+    const mapToSongState = (result) => {
+        let songs = [];
+        result.items.map((item) => {
+            const song = {
+                album: {
+                    name: item.album.name,
+                    thumbnail: item.album.images[0].url
+                },
+                song: {
+                    name: item.name,
+                    artist: item.artists[0].name,
+                    preview: item.preview_url
+                }
+            }
+            songs.push(song);
+        });
+        setMostPlayed(songs);
     }
 
     useEffect(() => {
-        async function loadData() {
-            const result = await load();
-            console.log('Most Played: ', result);
+        const loadData = async () => {
+            await load();
+            // mapToSongState(result);
         }
         loadData();
-    });
+        console.log('State MostPlayed: ', mostPlayed);
+    }, []);
 
     return (
         <div className='dashboard-container'>
-            <div style={{ display: 'flex', justifyContent: 'space-evenly', alignItems: 'center', width: '100%' }}>
-                <div style={{ display: 'flex', height: 300, backgroundColor: 'lightgrey', width: 250, borderRadius: 10, padding: 10 }}>
-                    <p>Most PLayed</p>
-                    <div style={{backgroundColor: 'green', height: '100%'}}>
-                        <p>This is Image Section</p>
-                    </div>
-                    <div style={{backgroundColor: 'yellow', height: '100%'}}>
-                        This is Info Section
+            <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', width: '100%' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', height: 300, backgroundColor: 'lightgrey', width: 250, borderRadius: 10, padding: 10 }}>
+                    <div style={{ height: '100%', width: '100%', textAlign: 'center' }}>
+                        <img src={mostPlayed[0]?.album.thumbnail} style={{ height: 170, width: 170, borderRadius: '80px' }} alt="Album" />
+                        {/* <audio controls>
+                            <source src={mostPlayed[0]?.song.preview} type="audio/mpeg" />
+                        </audio> */}
+                        <p style={{ color: '#000', fontWeight: 700, fontSize: 'larger', marginTop: 10, marginBottom: 0 }}> {mostPlayed[0]?.song.name}</p>
+                        <p style={{ color: '#000', fontWeight: 400, fontSize: 'small' }}> {mostPlayed[0]?.song.artist}</p>
                     </div>
                 </div>
-                <div style={{ height: 300, backgroundColor: 'lightgrey', width: 250, borderRadius: 10, padding: 10 }}><p>This is Section Two</p></div>
-                <div style={{ height: 300, backgroundColor: 'lightgrey', width: 250, borderRadius: 10, padding: 10 }}><p>This is Section Three</p></div>
+                <div style={{ display: 'flex', flexDirection: 'column', height: 300, backgroundColor: 'lightgrey', width: 250, borderRadius: 10, padding: 10 }}><p>This is Section Two</p></div>
+                <div style={{ display: 'flex', flexDirection: 'column', height: 300, backgroundColor: 'lightgrey', width: 250, borderRadius: 10, padding: 10 }}><p>This is Section Three</p></div>
+                <div style={{ display: 'flex', flexDirection: 'column', height: 300, backgroundColor: 'lightgrey', width: 250, borderRadius: 10, padding: 10 }}><p>This is Section Four</p></div>
             </div>
             <div style={{ marginTop: 20, height: 175, backgroundColor: 'blue', width: 1000, borderRadius: 10, padding: 10 }}><p>This is Section Four</p></div>
-            {/* <p>Display Name: {profile.displayName}</p>
-                <p>Country: {profile.country}</p>
-                <p>Email: {profile.email}</p>
-                <p>
-                    <img alt = "Profile" src = {profile.picture} /></p>
-                <p>Product: {profile.product}</p>
-                <p>Type: {profile.type}</p>
-                <p>URI: {profile.uri}</p> */}
         </div>
     );
 }
