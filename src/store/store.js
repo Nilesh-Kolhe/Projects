e88d6a8bf62f1f71ce64a11807f7ec76823683b6
Nilesh Kolhe <nkolhe@vexcelco.com>
2024-03-26 22:58:14 +0530

    gh-pages

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import profileReducer from '../reducers/index'; 
-
-const store = configureStore({
-  reducer: profileReducer, 
-});
-
-export default store;
