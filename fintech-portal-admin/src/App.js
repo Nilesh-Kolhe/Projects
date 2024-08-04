8118de8d408b064b4f4ae42288fab78f462af45b
Nilesh Kolhe <kolhe.nilesh@rocketmail.com>
2024-08-04 21:03:01 +0530

    Added Login Page

--- fintech-portal-admin/src/App.js
+++ fintech-portal-admin/src/App.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import './App.css';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Enquiry from './pages/Enquiry';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TopMostNav from './components/TopMostNav';
@@ -7,23 +9,33 @@ import TopNav from './components/TopNav';
 import { Navigate } from 'react-router-dom';
 import SideNav from './components/SideNav';
 
-function App() {
+const App = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="App" style={{ height: '100%', width: '100%' }}>
       <BrowserRouter>
         <TopMostNav />
         <TopNav />
-        <div style={{ display: 'flex', columnGap: '50px', height: '100%', width: '100%', margin: '0px 0px', overflow: 'auto' }}>
-          <SideNav />
-          <div style={{ width: '100%', marginTop: '10px' }}>
-            <Routes>
-              <Route path="/" element={<Navigate to="login" />} />
-              {/* <Route path="login" element={<Login />} /> */}
-              <Route path="home" element={<Home />} />
-              <Route path="enquiries" element={<Enquiry />} />
-            </Routes>
-          </div>
-        </div>
+        {isLoggedIn ?
+          <>
+            <div style={{ display: 'flex', columnGap: '50px', height: '100%', width: '100%', margin: '0px 0px', overflow: 'auto' }}>
+              <SideNav />
+              <div style={{ width: '100%', marginTop: '10px' }}>
+                <Routes>
+                  <Route path="/" element={<Navigate to="home" />} />
+                  {/* <Route path="login" element={<Login />} /> */}
+                  <Route path="home" element={<Home />} />
+                  <Route path="enquiries" element={<Enquiry />} />
+                </Routes>
+              </div>
+            </div>
+          </>
+          :
+          <>
+            <Login loginChange={(loginStatus) => setIsLoggedIn(loginStatus)} />
+          </>}
       </BrowserRouter >
     </div>
   );
