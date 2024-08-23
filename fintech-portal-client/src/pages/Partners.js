f58e4902d3e3e4bbf14c6920fce15c52dd8daa32
Nilesh Kolhe <kolhe.nilesh@rocketmail.com>
2024-08-23 21:10:14 +0530

    Responsive About Us

--- fintech-portal-client/src/pages/Partners.js
+++ fintech-portal-client/src/pages/Partners.js
@@ -23,8 +23,8 @@ const Partners = () => {
     }, []);
 
     return (
-        <div id="partners" style={{ display: 'flex', flexDirection: 'column', rowGap: '30px', alignItems: 'center', width: '100%', height: '700px' }}>
-            <span id='headline' style={{ fontWeight: '500', fontVariantCaps: 'petite-caps', padding: '30px 2px' }} >Our Partners</span>
+        <div id="partners">
+            <span id='headline'>Our Partners</span>
             <span id='sub-heading' style={{ fontStyle: 'italic', fontWeight: '100' }} >&quot;Empowering your financial journey with trusted partnership from leading banks.&quot;</span>
             <div style={{ display: 'flex', rowGap: '40px', flexDirection: 'column', alignItems: 'center' }}>
                 <div>
